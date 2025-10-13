334b3dd1713246a0c3633fa1ae9f88c81f42c0e4
Shayan Khan <shynkhn17@gmail.com>
2025-10-13 12:06:07 +0500

    changes

--- cstm-react/cstmreact.js
+++ cstm-react/cstmreact.js
@@ -1,19 +1,19 @@
 function renderreact(reactElement , container){
     const domElement = document.createElement(reactElement.type)
-    domElement.innerHTML = reactElement.Children
+    domElement.innerHTML = reactElement.children
     domElement.setAttribute('href',reactElement.props.href)
     domElement.setAttribute('target',reactElement.props.target)
-    container.apendchild(domElement)
+    container.appendChild(domElement);
 }
 
 
 const reactElement={
     type :'a',
     props:{
-        href :'https:/google.com',
+        href : 'https://google.com',
         target : '_blank',
     }    ,
-    Children: 'click me to visit google'
+    children: 'click me to visit google'
 }
 const maincontainer = document.getElementById('root');
 
