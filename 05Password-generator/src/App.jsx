16bb2473468df6a0f17a765dbdf36fbaac04edc7
Shayan Khan <shynkhn17@gmail.com>
2025-11-04 14:39:34 +0500

    password generator progjec

--- 05Password-generator/src/App.jsx
+++ 05Password-generator/src/App.jsx
@@ -1,25 +1,37 @@
 import './assets/cstm.css'
 import React from 'react'
-import { useState, useCallback } from 'react'
+import { useState, useCallback ,useEffect , useRef} from 'react'
 
 function App() {
   const [length, setlength] = useState(8);
   const [num, setnum] = useState(false)
   const [char, setchar] = useState(false)
   const [Password, setpassword] = useState("")
+
+  // USE CALLBACK
   const passgen = useCallback(() => {
     let pass = "";
-    let str = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     if (num) str += "0123456789";
-    if (char) str += "! @ # $ % ^ & * ( ) _ - + = { } [ ] | \ : ;' < > , . ? / ~ `"
-    for (i = 1; i <= array.length; i++) {
-      let char = (Math.floor(Math.random() * str.length + 1))
-      pass += str.charAt(char)
+    if (char) str += "!@#()_-+={}[]|\$<>,?/~"
+    for (let i = 1; i <= length; i++) {
+      let chars = (Math.floor(Math.random() * str.length ))
+      pass += str.charAt(chars)
     }
     setpassword(pass)
 
-  }, [length, num, char, setpassword])
-  console.log(Password)
+  }, [length, num, char])
+  
+  // USE EFFECT
+  useEffect(() => {
+    passgen()
+  }, [length, num, char])
+
+  // USE REF
+  let passwordRef = useRef(null)
+  let copypass = useCallback(() =>{
+    window.navigator.clipboard.writeText(Password)
+  },[Password])
   return (
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 ">
@@ -28,30 +40,43 @@ function App() {
         <div className="flex shadow rounded-lg overflow-hidden my-4 ">
           <input
             type="text"
-            className='outline-none w-full px-3 py-2  bg-white'
+            className='outline-none text-orange-500 w-full px-3 py-2  bg-white'
             value={Password}
             placeholder="Your Password"
+            ref={passwordRef}
           />
-          <button className='text-white bg-blue-500 py-0.5 px-3 outline-none shrink-0 fw-bold cursor-pointer'>COPY</button>
+          <button onClick ={copypass} className='text-white bg-blue-500 py-0.5 px-3 outline-none shrink-0 fw-bold cursor-pointer'>COPY</button>
         </div>
 
-        <div className="flex justify-between text-white my-2">
+        <div className="flex justify-between text-white my-2 ">
           <input
             type="range"
-            min="4"
+            min="8"
             max="100"
             value={length}
             className='accent-blue-500 cursor-pointer '
-            onChange={(e) => {setlength(e.target.value)}}
+            onChange={(e) => { setlength(Number(e.target.value)) }}
           />
-          <label> Length : {length}</label>
+          <label className='text-orange-500'> Length : {length}</label>
           <div className='flex items-center gap-x-1'>
-            <input 
+            <input
               type="checkbox"
               defaultChecked={num}
-              onChange={() => setnum(prev => !prev)}
+              onChange={() => setnum((prv) => !prv)}
               className="bg-black hover:bg-white"
+              id='number'
             />
+            <label htmlFor="number" className="text-orange-500">Number</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input
+              type="checkbox"
+              defaultChecked={char}
+              onChange={() =>setchar(prev => !prev)}
+              className="bg-black hover:bg-white"
+              id='setchar'
+            />
+            <label htmlFor="setchar" className="text-orange-500">Characters</label>
           </div>
         </div>
       </div>
