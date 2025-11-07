2ea7216fe45c5d1aa2cf3a18eb0c9242f1eb6aef
Shayan Khan <shynkhn17@gmail.com>
2025-11-07 11:53:31 +0500

    change

--- 06Cleanpassgen/src/components/card.jsx
+++ 06Cleanpassgen/src/components/card.jsx
@@ -8,27 +8,27 @@ export default function Card(props) {
     return (
         <>
 
-            <div className='card-div flex justify-between mt-5 w-220 m-auto border-[1.5px] p-4 rounded-lg'>
+            <div className='card-div flex justify-between mt-5 w-220 maincard m-auto  rounded-lg'>
                 <input
                     type="text"
-                    className='bg-white border-none w- text-3xl flex-grow  rounded-lg focus:outline-none'
+                    className='passinput bg-white border-[1.5px] p-4 p-md-2 text-3xl flex-grow  rounded-lg focus:outline-none'
                     placeholder='Your Password'
                     Value={props.Password}
-                    ref = {props.reference}
+                    ref={props.reference}
                 />
-                <div className=" flex gap-x-4">
+                <div className=" flex gap-x-4 ml-4 btn-div">
                     <button
                         onClick={() => { props.passgen((prev) => !prev) }}
                     >
                         <FontAwesomeIcon
                             className='text-1xl cursor-pointer text-gray-600'
-                            icon={faRotate} 
+                            icon={faRotate}
                         />
                     </button>
 
                     <button
-                    className='btn bg-[#d71340] cursor-pointer p-3 font-bold text-white rounded-lg'
-                    onClick={props.copy}
+                        className='btn bg-[#d71340] cursor-pointer p-3 font-bold text-white rounded-lg'
+                        onClick={props.copy}
                     >Copy Password</button>
                 </div>
 
@@ -49,9 +49,10 @@ export default function Card(props) {
                 </div>
                 <div className=" text-start flex items-center jusify-between mt-7">
                     <label htmlFor="char" className='font-bold text-1xl w-54'>Characters Used</label>
-                    <div className=" flex items-center">
+                    <div className="inputs flex items-center">
 
-                        <input
+                       <div>
+                         <input
                             onChange={() => { props.setupperchar((prev) => !prev) }}
                             type="checkbox"
                             id="upper"
@@ -59,23 +60,29 @@ export default function Card(props) {
                         />
                         <label htmlFor="upper" onChange={() => { props.setupperchar((prev) => !prev) }} className='mr-5 ml-2 font-300 cursor-pointer'>Uppercase</label>
 
+                       </div>
                         {/* <input onClick={(e) => setupperchar((prev) => !prev)}
                             type="checkbox" id="lower"
                             className='appearance-none w-5 h-5 border border-gray-500 rounded-sm checked:bg-blue-500 cursor-pointer'
                         />
                         <label htmlFor="lower" className='mr-5 ml-2 font-300 cursor-pointer'>Lowercase</label> */}
+                        <div>
 
-                        <input onClick={() => { props.setnum((prev) => !prev) }}
-                            type="checkbox" id="num"
-                            className=' w-5 h-5 cursor-pointer'
-                        />
-                        <label htmlFor="num" className='mr-5 ml-2 font-300 cursor-pointer'>Numbers</label>
+                            <input onClick={() => { props.setnum((prev) => !prev) }}
+                                type="checkbox" id="num"
+                                className=' w-5 h-5 cursor-pointer'
+                            />
+                            <label htmlFor="num" className='mr-5 ml-2 font-300 cursor-pointer'>Numbers</label>
 
-                        <input onClick={() => { props.setsymbols((prev) => !prev) }}
-                            type="checkbox" id="symbol"
-                            className=' w-5 h-5  cursor-pointer'
-                        />
-                        <label htmlFor="symbol" className='mr-5 ml-2 font-300 cursor-pointer'>Symbols</label>
+                        </div>
+
+                        <div>
+                            <input onClick={() => { props.setsymbols((prev) => !prev) }}
+                                type="checkbox" id="symbol"
+                                className=' w-5 h-5  cursor-pointer'
+                            />
+                            <label htmlFor="symbol" className='mr-5 ml-2 font-300 cursor-pointer'>Symbols</label>
+                        </div>
                     </div>
 
 
--- 06Cleanpassgen/src/components/pageheadtext.jsx
+++ 06Cleanpassgen/src/components/pageheadtext.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function PageHead(props) {
     return (
         <div className='max-w-240 mx-auto headerdiv'>
-            <h1 className={`font-${props.tect}`}>{props.h1}</h1>
-            <h3 className="text-5xl mb-6 font-normal">
+            <h1 className={`font-${props.tect} headertext`}>{props.h1}</h1>
+            <h3 className="text-5xl mb-6 font-normal headertext">
                 Instantly generate a secure, random password with the LastPass online tool
             </h3>
-            <p className='text-3xl'>Use our online password generator tool to instantly create a secure, random password.</p>
+            <p className='text-3xl headerp'>Use our online password generator tool to instantly create a secure, random password.</p>
         </div>
     )
 }
