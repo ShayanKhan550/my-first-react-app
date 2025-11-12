2fd1965fea826c61d0dc780f699af849e33edfb5
Shayan Khan <shynkhn17@gmail.com>
2025-11-12 16:28:12 +0500

    change

---
+++ 07curencyconvorter/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import InputBox from './components/InputBox'
+function App() {
+
+
+  return (
+    <>
+    <InputBox/>
+      <h1 className="text-4xl text-center p-3 bg-orange-500">Welcom To Currency Convertor</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 07curencyconvorter/src/components/InputBox.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function InputBox() {
+   
+  return (
+    <h3>fdjs</h3>
+  );
+}
+
+export default InputBox
