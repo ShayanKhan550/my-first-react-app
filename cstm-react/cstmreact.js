b6de24a23bc8dc54a19224a7d644ef9223681086
Shayan Khan <shynkhn17@gmail.com>
2025-11-27 17:21:03 +0500

    changes

--- cstm-react/cstmreact.js
+++ cstm-react/cstmreact.js
@@ -1,98 +1,149 @@
-// // function renderreact(reactElement , container){
-// //     const domElement = document.createElement(reactElement.type)
-// //     domElement.innerHTML = reactElement.children
-// //     domElement.setAttribute('href',reactElement.props.href)
-// //     domElement.setAttribute('target',reactElement.props.target)
-// //     container.appendChild(domElement);
-// // }
+// // // // function renderreact(reactElement , container){
+// // // //     const domElement = document.createElement(reactElement.type)
+// // // //     domElement.innerHTML = reactElement.children
+// // // //     domElement.setAttribute('href',reactElement.props.href)
+// // // //     domElement.setAttribute('target',reactElement.props.target)
+// // // //     container.appendChild(domElement);
+// // // // }
 
 
-// function renderreact(reactElement , container){
-// const domElement = document.createElement(reactElement.type)
-// domElement.innerHTML = reactElement.children
-// for(const prop in reactElement.props){
-//     if(prop === 'children') continue ;
-//     domElement.setAttribute(prop , reactElement.props [prop])
+// // // function renderreact(reactElement , container){
+// // // const domElement = document.createElement(reactElement.type)
+// // // domElement.innerHTML = reactElement.children
+// // // for(const prop in reactElement.props){
+// // //     if(prop === 'children') continue ;
+// // //     domElement.setAttribute(prop , reactElement.props [prop])
+// // // }
+// // //     container.appendChild(domElement);
+// // // }
+
+// // // const reactElement={
+// // //     type :'a',
+// // //     props:{
+// // //         href : 'https://google.com',
+// // //         target : '_blank',
+// // //     }    ,
+// // //     children: 'click me to visit google'
+// // // }
+// // // const maincontainer = document.getElementById('root');
+
+// // // renderreact(reactElement , maincontainer)
+
+// // function callback(a,b){
+// //     setTimeout(()=>{
+// //         const ul = document.getElementsByClassName("print")[0]
+// //         const li = document.createElement('li')
+// //         li.innerText =a 
+// //         ul.appendChild(li)
+// //         console.log(a);
+// //            if(b){
+// //             b()
+// //             }
+// //     },1000)
+
+// //     }
+// // callback(1,()=>{
+// //     callback(2,()=>{
+// //         callback(3,()=>{
+// //             callback(4,()=>{
+// //                 callback(5,()=>{
+// //                      callback(6,()=>{
+// //                          callback(7,()=>{
+// //                              callback(8,()=>{
+// //                                  callback(9,()=>{
+// //                                      callback(10,()=>{
+// //                                          callback(11,()=>{
+// //                                              callback(12,()=>{
+// //                                                  callback(13,()=>{
+// //                                                      callback(14,()=>{
+// //                                                          callback(15,()=>{
+// //                                                              callback(16,()=>{
+// //                                                                 callback(17)
+// //                                                              })
+// //                                                          })
+// //                                                      })
+// //                                                  })
+// //                                              })
+// //                                          })
+// //                                      })
+// //                                  })
+// //                              })
+// //                          })
+// //                      })
+// //                 })
+// //             })
+// //         })
+// //     })
+// // })
+// // // Task A Random nm 32 print it for a 4 times in sequence
+// function random(a, b) {
+//     setTimeout(() => {
+//         const ul = document.getElementsByClassName('print')[0];
+//         const li = document.createElement('li');
+//         let random = Math.floor(Math.random() * 100)
+//         a += random
+//         ul.innerText = a
+//         // ul.appendChild(li)
+//         if (b) b();
+//     }, 1000);
 // }
-//     container.appendChild(domElement);
+// random("Task A Random :", () => {
+//     random("Task B Random :", () => {
+//         random("Task C Random :",()=>{
+//             random("Task D Random :", ()=>{
+//                 random("Task E Random :")
+//             } )
+//         })
+//     })
+// })
+// console.log("start");
+// function nested(a,b) {
+//     setTimeout(() => {
+//         const div = document.getElementsByClassName('div')[0];
+//         div.style.fontSize='53px'
+//         div.innerText = a;
+//         if (b) b()
+//     }, 2000)
 // }
 
-// const reactElement={
-//     type :'a',
-//     props:{
-//         href : 'https://google.com',
-//         target : '_blank',
-//     }    ,
-//     children: 'click me to visit google'
-// }
-// const maincontainer = document.getElementById('root');
 
-// renderreact(reactElement , maincontainer)
+// nested("step 1",()=>{
+//     nested("step 2",()=>{
+//         nested("step 3",()=>{
+//             nested("step 4")
+//         })
+//     })
+// })
 
-function callback(a,b){
+// CAll back hell 
+function lets(a,b){
     setTimeout(()=>{
-        const ul = document.getElementsByClassName("print")[0]
-        const li = document.createElement('li')
-        li.innerText =a 
-        ul.appendChild(li)
-        console.log(a);
-           if(b){
-            b()
-            }
-    },1000)
-
-    }
-callback(1,()=>{
-    callback(2,()=>{
-        callback(3,()=>{
-            callback(4,()=>{
-                callback(5,()=>{
-                     callback(6,()=>{
-                         callback(7,()=>{
-                             callback(8,()=>{
-                                 callback(9,()=>{
-                                     callback(10,()=>{
-                                         callback(11,()=>{
-                                             callback(12,()=>{
-                                                 callback(13,()=>{
-                                                     callback(14,()=>{
-                                                         callback(15,()=>{
-                                                             callback(16,()=>{
-                                                                callback(17)
-                                                             })
-                                                         })
-                                                     })
-                                                 })
-                                             })
-                                         })
-                                     })
-                                 })
-                             })
-                         })
-                     })
-                })
-            })
-        })
-    })
-})
-// Task A Random nm 32 print it for a 4 times in sequence
-function random(a, b) {
-    setTimeout(() => {
-        const ul = document.getElementsByClassName('print')[0];
-        const li = document.createElement('li');
-        let random = Math.floor(Math.random() * 100)
-        a += random
-        li.innerText = a
-        ul.appendChild(li)
-        if (b) b();
-    }, 1000);
+            console.log(a)
+    if(b) b()
+    },2000)
 }
-random("Task A Random :", () => {
-    random("Task B Random :", () => {
-        random("Task C Random :",()=>{
-            random("Task D Random :", ()=>{
-                random("Task E Random :")
-            } )
+lets("num 1" ,()=>{
+    lets("num 2", ()=>{
+        lets("num 3" , ()=>{
+            lets("num 4")
         })
     })
 })
+
+
+  const form = document.getElementById("myForm");
+  form.addEventListener("submit", function(e){
+    e.preventDefault(); // default submit roko
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    if(name === "" || email === ""){
+      alert("Please fill all fields!");
+      let p = document.createElement('p')
+      p.innerText= 'its error motherfucker plz enter name gandu'
+        form.appendChild(p)
+        
+    } else {
+      alert("Form submitted successfully!");
+    }
+  });
+  
